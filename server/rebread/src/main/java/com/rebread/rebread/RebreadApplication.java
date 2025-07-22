33a2ae66e05b8acd5eb3a6e1fd0663827700ea41
dudasdaily <dudasdaily@gmail.com>
2025-07-22 19:42:15 +0900

    spring boot 프로젝트 생성

---
+++ server/rebread/src/main/java/com/rebread/rebread/RebreadApplication.java
@@ -0,0 +1,13 @@
+package com.rebread.rebread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RebreadApplication.class, args);
+	}
+
+}
---
+++ server/rebread/src/test/java/com/rebread/rebread/RebreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebread.rebread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
